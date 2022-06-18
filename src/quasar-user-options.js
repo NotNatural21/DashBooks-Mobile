22b78db6e24b5f8b2fa2bcfa2fd168400380bb90
NotNatural21 <89242396+NotNatural21@users.noreply.github.com>
2022-06-19 11:06:58 +1200

    Create quasar-user-options.js

---
+++ src/quasar-user-options.js
@@ -0,0 +1,13 @@
+
+import './styles/quasar.scss'
+import iconSet from 'quasar/icon-set/material-icons-outlined.js'
+import '@quasar/extras/roboto-font/roboto-font.css'
+import '@quasar/extras/material-icons-outlined/material-icons-outlined.css'
+
+// To be used on app.use(Quasar, { ... })
+export default {
+  config: {},
+  plugins: {
+  },
+  iconSet: iconSet
+}
