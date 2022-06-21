a98ab410fa78e5be78d52d33efb6186b71f7a372
NotNatural21 <89242396+NotNatural21@users.noreply.github.com>
2022-06-22 11:40:44 +1200

    added api

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,12 @@
 const { defineConfig } = require('@vue/cli-service');
+const Dotenv = require('dotenv-webpack');
 module.exports = defineConfig({
     configureWebpack: {
         experiments: {
             topLevelAwait: true,
-        }
-    }
+        },
+        plugins: [
+            new Dotenv()
+        ]
+    },
 })
